81f417eb53fd726537f2268b19bb49971e41bada
Vincent Gauthier <vincent.gauthier@telecom-sudparis.eu>
2020-02-02 18:03:55 +0100

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,19 @@
+push!(LOAD_PATH,"../src/")
 using Documenter, JGraph
 
 makedocs(;
     modules=[JGraph],
-    format=Documenter.HTML(prettyurls = true),
+    format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
     ],
-    repo="https://github.com/vgauthier/JGraph.jl/blob/{commit}{path}#L{line}",
     sitename="JGraph.jl",
     authors="Vincent Gauthier",
-    assets=String[],
+    doctest  = false,
 )
 
 deploydocs(;
-    repo="github.com/vgauthier/JGraph.jl",
+    repo="github.com/vgauthier/JGraph.jl.git",
+    branch = "gh-pages",
+    target = "build",
 )
