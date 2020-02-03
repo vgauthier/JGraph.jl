2e21a017f3d54b275cb623c3a90e2d85e8569031
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-03 21:38:54 +0000

    build based on 4d91a22

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#JGraph.jl-1","page":"Home","title":"JGraph.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [JGraph]","category":"page"},{"location":"#JGraph.greet-Tuple{}","page":"Home","title":"JGraph.greet","text":"greet()\n\nReturns \"hello world!\"\n\nExamples\n\njulia> using JGraph \n\njulia> greet()\n\"Hello World!\"\n\n\n\n\n\n","category":"method"}]
+[{"location":"#JGraph.jl-1","page":"Home","title":"JGraph.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [JGraph]","category":"page"}]
 }
