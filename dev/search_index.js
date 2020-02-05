8f87c401411f9078fca77d0a6eed79b45aed8306
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-05 10:56:42 +0000

    build based on 30de5bc

--- dev/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#JGraph.jl-1","page":"Home","title":"JGraph.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [JGraph]","category":"page"}]
-}
